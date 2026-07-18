282ea31256025b722da2879d5333902e37c94e59
Moretz (Fedora) <moritzvaneimern@gmail.com>
2026-07-18 19:37:08 +0900

    prepare windows

--- src/moretzslmcontrol/monitor_stuff/platform/windows.py
+++ src/moretzslmcontrol/monitor_stuff/platform/windows.py
@@ -10,6 +10,7 @@ from typing import TYPE_CHECKING
 
 import pyedid
 
+from moretzslmcontrol.monitor_stuff.models import ScreenDescriptor
 from moretzslmcontrol.monitor_stuff.platform import PlatformAdapter
 from moretzslmcontrol.monitor_stuff.platform.windows_edid import request_windows_edids
 from pyedid import Edid
@@ -21,9 +22,16 @@ if TYPE_CHECKING:  # Type hinting imports in here when cyclic imports occur
 
 
 class WindowsPlatformAdapter(PlatformAdapter):
-    @staticmethod
-    def get_screen_edid() -> list[Edid]:
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.windows_monitors = request_windows_edids()
+
+    def get_screen_edid(self, descriptor: ScreenDescriptor) -> list[Edid]:
         win_monitors = request_windows_edids()
+
+        
+
         return [pyedid.parse_edid(win_monitor.edid) for win_monitor in win_monitors]
 
     def detach_window_from_screen(self, window: QWidget) -> None:
@@ -32,3 +40,6 @@ class WindowsPlatformAdapter(PlatformAdapter):
 
     def keep_window_in_focus(self, window: QWidget) -> None:
         super().keep_window_in_focus(window)
+
+    def refresh_edid_list(self) -> None:
+        self.windows_monitors = request_windows_edids()
